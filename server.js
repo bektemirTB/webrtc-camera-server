3f8f92583beef8fd9d0694255c7e3965adbd791c
bektemirTB <bektemir.t.b@gmail.com>
2025-10-31 23:11:11 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public")); // тут потом будут html файлы
+
+io.on("connection", (socket) => {
+  console.log("Новое подключение:", socket.id);
+
+  socket.on("offer", (data) => socket.broadcast.emit("offer", data));
+  socket.on("answer", (data) => socket.broadcast.emit("answer", data));
+  socket.on("ice-candidate", (data) => socket.broadcast.emit("ice-candidate", data));
+
+  socket.on("disconnect", () => console.log("Отключился:", socket.id));
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Сервер запущен: http://localhost:${PORT}`);
+});
